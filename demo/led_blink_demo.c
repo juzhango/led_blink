8520a18c2a44aacf9716ef1d6f59df20762e2299
luozhengwu <luozhengwu@solar-lit.com>
2025-06-20 14:42:39 +0800

    update

--- demo/led_blink_demo.c
+++ demo/led_blink_demo.c
@@ -3,12 +3,15 @@
 #include <board.h>
 #include <led_blink.h>
 
+#define LED_PIN GET_PIN(E, 8)
+#define LED_ACTIVE_LEVEL PIN_LOW
+
 st_led_node_t *led1 = RT_NULL;
 static int led_blink_demo_init(void)
 {
     st_led_pin_t led_pin;
-    led_pin.pin = PKG_LED_BLINK_DEMO_GPIO_PIN;
-    led_pin.active_level = PKG_LED_BLINK_DEMO_ACTIVE_LEVEL;
+    led_pin.pin = LED_PIN;
+    led_pin.active_level = LED_ACTIVE_LEVEL;
 
     led1 = led_register(&led_pin, 50, 950, -1);
     return 0;
